f20e6f6d24c7ab818f4f23cad8ec0f0d3b8d1617
Ru Cindrea <ru.cindrea@altom.ro>
2014-11-14 10:33:43 +0200

    Updated iOS example to automatically detect platformName and testdroidTarget
    Conflicts:
    	src/test/java/com/testdroid/appium/samples/ios/BitbarIOSSampleTest.java

--- src/test/java/com/testdroid/appium/samples/ios/BitbarIOSSampleTest.java
+++ src/test/java/com/testdroid/appium/samples/ios/BitbarIOSSampleTest.java
@@ -36,8 +36,6 @@ public class BitbarIOSSampleTest {
         // You can override the the in testdroid.properties or give file UUID
         client.setAppFile(new File(SAMPLE_APP_PATH));
         client.setBundleId(BUNDLE_ID);
-        client.setPlatformName(TestdroidAppiumClient.APPIUM_PLATFORM_IOS);
-        client.setTestdroidTarget(TestdroidAppiumClient.TESTDROID_TARGET_IOS);
         // Wait one hour for free device
         client.setDeviceWaitTime(3600);
 
